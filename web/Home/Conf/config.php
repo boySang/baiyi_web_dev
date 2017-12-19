c723450bf4712abeb8301b7290a51096bc3ec3f6
boySang <sxsjl2015@126.com>
2017-12-20 00:52:50 +0800

    隐藏index.php

--- web/Home/Conf/config.php
+++ web/Home/Conf/config.php
@@ -1,4 +1,15 @@
 <?php
 return array(
 	//'配置项'=>'配置值'
+
+	//url访问模式为rewrite模式
+	'URL_MODEL'=>'2',
+	//开启伪静态
+	'URL_HTML_SUFFIX' =>'.html',
+	//开启路由
+	'URL_ROUTER_ON' =>true,
+	//路由规则
+	'URL_ROUTE_RULES'=>array(
+		// 'news/:id' => 'News/read',
+	),
 );
